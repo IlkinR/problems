0ce22616e9a414ed338bcd1656c8a984bb366283
Ilkin Ramazanov <babalke44@gmail.com>
2021-09-20 00:16:39 +0400

    implement contains duplicates

--- problems.py
+++ problems.py
@@ -1,6 +1,10 @@
-def problem1():
-    pass
+from typing import List
 
 
 def add(num1: int, num2: int):
     return num1 + num2
+
+
+def contains_duplicates(nums: List[int]) -> bool:
+    snums = sorted(nums)
+    return any(snums[i] == snums[i + 1] for i in range(len(snums) - 1))
--- test_problems.py
+++ test_problems.py
@@ -1,4 +1,4 @@
-from problems import add
+from problems import add, contains_duplicates
 import unittest
 
 
@@ -7,6 +7,11 @@ class TestProblems(unittest.TestCase):
         self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(-1, 2), -1)
         self.assertEqual(add(0, 2), 2)
+
+    def test_contains_duplicates(self):
+        self.assertEqual(contains_duplicates([1, 2, 3, 1]), True)
+        self.assertEqual(contains_duplicates([1, 2, 3, 4]), False)
+        self.assertEqual(contains_duplicates([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]), True)
 
 
 if __name__ == "__main__":
